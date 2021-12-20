f8f33d7c0edb07cabdb08a988eeb91e90baed1be
Morad Alee <moradalee.programmer@gmail.com>
2021-12-20 16:03:03 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using School_Management_System.UI;
+using School_Management_System.Reporting;
+
+namespace School_Management_System
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Gov__High_School_Topsin  ());
+        }
+    }
+}
